6becc7532badcda5ec15f05433f23a72c7c1e5e3
Developerayo <20538832+Developerayo@users.noreply.github.com>
2019-03-03 12:16:35 -0800

    Resolved gatby-plugin-manifest and Tracking ID (#12)
    Resolved gatby-plugin-manifest and Tracking ID (#12)[https://github.com/Developerayo/shodipoayomide.com/issues/12]

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,15 +50,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-125265218-1`,
       },
     },
     `gatsby-plugin-feed`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `663399663399`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Developerayo`,
+        short_name: `Developerayo`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
