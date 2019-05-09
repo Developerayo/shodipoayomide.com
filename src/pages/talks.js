6e6cec39acd444fa45f958239cd4576417859c2f
Developerayo <20538832+Developerayo@users.noreply.github.com>
2019-05-09 19:49:03 +0400

    feat: talks.js new page

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -13,25 +13,23 @@ class talks extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
       <SEO title="Talks" />
       <h1>Upcoming Talks</h1>
+      <p>1)<b>Conference Name:</b> Wordpress Conferenc,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
+      <b>Topic:</b> Remote Work <br />&nbsp;&nbsp;&nbsp;<b>Date:</b> 11th January 2019
+      </p>
+
+      <h1>Past Talks</h1>
       <p>1)<b>Conference Name:</b> Full Stack Developers Conference,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lekki, Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
       <b>Topic:</b> Getting started with GraphQL (From Zero to Hero)<br />&nbsp;&nbsp;&nbsp;<b>Date:</b> 4th Janyary 2019
       </p>
 
-      <p>2)<b>Conference Name:</b> Wordpress Conference,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
-      <b>Topic:</b> Remote Work on a new level<br />&nbsp;&nbsp;&nbsp;<b>Date:</b> 11th Janyary 2019
+      <p>2)<b>Conference Name:</b> NodeJS Conference,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;</p>
+      <p>3)<b>Conference Name:</b> DevFest Lagos,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
       </p>
-
-      <h1>Past Talks</h1>
-      <p>1)<b>Conference Name:</b> NodeJS Conference,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;</p>
-      <p>2)<b>Conference Name:</b> DevFest Lagos,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
+      <p>4)<b>Conference Name:</b> DevFest,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
       </p>
-      <p>3)<b>Conference Name:</b> DevFest,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
-      </p>
-      <p>4)<b>Conference Name:</b> DevFest Ikorodu,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
+      <p>5)<b>Conference Name:</b> DevFest Ikorodu,<br /> &nbsp;&nbsp;&nbsp;<b>City:</b> Lagos, Nigeria,<br />&nbsp;&nbsp;&nbsp;
       </p>
       <center><p>...</p></center>
-
-
 
       </Layout>
     )
