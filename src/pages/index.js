438ca83812b2170c69ba2e10eeddf88ebbd0f3b7
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2019-11-11 02:41:54 +0400

    fix: deploymen t to zeit now
    fix: deploymen t to zeit now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import {Helmet} from "react-helmet";
+import {Helmet} from "react-helmet"; 
 
 // import Bio from "../components/tag"
 import Bio from "../components/bio"
