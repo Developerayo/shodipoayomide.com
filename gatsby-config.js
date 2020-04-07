1c9c96f6d3d8f14f32b85ec2d24574327f865bc2
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-04-07 17:54:46 +0100

    merge this nonsense

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,88 +1,85 @@
-module.exports = {
-  siteMetadata: {
-    title: `Developerayo`,
-    author: `Shodipo Ayomide`,
-    description: `Articles on things frontend, Software Engineering by Developerayo. Sr.Developer Advocate & Software Engineer.`,
-    siteUrl: `http://shodipoayomide.com`,
-    social: {
-      twitter: `developerayo`,
+export const siteMetadata = {
+  title: `Developerayo`,
+  author: `Shodipo Ayomide`,
+  description: `Articles on things frontend, Software Engineering by Developerayo. Sr.Developer Advocate & Software Engineer.`,
+  siteUrl: `http://shodipoayomide.com`,
+  social: {
+    twitter: `developerayo`,
+  },
+};
+export const plugins = [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/content/blog`,
+      name: `blog`,
     },
   },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/content/assets`,
+      name: `assets`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 590,
           },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
+        },
+        {
+          resolve: `gatsby-remark-responsive-iframe`,
+          options: {
+            wrapperStyle: `margin-bottom: 1.0725rem`,
           },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: false,
-            },
+        },
+        {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            classPrefix: "language-",
+            inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+            noInlineHighlight: false,
           },
-
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
+        },
+        `gatsby-remark-copy-linked-files`,
+        `gatsby-remark-smartypants`,
+      ],
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-125265218-1`,
-      },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: `UA-125265218-1`,
     },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Developerayo`,
-        short_name: `Developerayo`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
+  },
+  `gatsby-plugin-feed`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Developerayo`,
+      short_name: `Developerayo`,
+      start_url: `/`,
+      background_color: `#ffffff`,
+      theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `content/assets/gatsby-icon.png`,
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+  },
+  `gatsby-plugin-offline`,
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography`,
     },
-  ],
-}
+  },
+];
