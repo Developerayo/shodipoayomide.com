ad0dcde3f675e4ab7adcb5404aa6381c4b8c35f3
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-04-17 13:23:15 +0100

    set color parameters

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -3,7 +3,7 @@ import novelaTheme from '@narative/gatsby-theme-novela/src/gatsby-plugin-theme-u
 
 export default {
   ...novelaTheme,
-  initialColorMode: `dark`,
+  initialColorMode: `initialColorMode`,
   colors: {
     ...novelaTheme.colors,
     primary: 'black',
@@ -11,6 +11,35 @@ export default {
     accent: 'grey',
     grey: 'black',
     background: 'white',
-    gradient: "linear-gradient(180deg, rgba(325, 426, 326, 0) 0%, lightgrey 100%)",
+    gradient: 'linear-gradient(180deg, rgba(325, 426, 326, 0) 0%, lightgrey 100%)',
+    track: "grey",
+    progress: 'green',
+    card: "#1D2128",
+    error: "#EE565B",
+    success: "#46B17B",
+    errorBackground: "rgba(238, 86, 91, 0.1)",
+    horizontalRule: "rgba(255, 255, 255, 0.15)",
+    inputBackground: "rgba(255, 255, 255, 0.07)",
+    modes: {
+      dark: {
+        grey: "white",
+        primary: "#fff",
+        secondary: "#fff",
+        accent: "lightgrey",
+        background: "#0D2538",
+        hover: "rgba(255, 255, 255, 0.07)",
+        gradient:
+          "linear-gradient(180deg, #0D2538 0%, rgba(66, 81, 98, 0.36) 100%)",
+        articleText: "#fff",
+        track: "white",
+        progress: "green",
+        card: "#1D2128",
+        error: "#EE565B",
+        success: "#46B17B",
+        errorBackground: "rgba(238, 86, 91, 0.1)",
+        horizontalRule: "rgba(255, 255, 255, 0.15)",
+        inputBackground: "rgba(255, 255, 255, 0.07)",
+      },
+    },
   },
 };
