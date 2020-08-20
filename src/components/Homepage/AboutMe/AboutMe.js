0a537cf8f8c3bf33c86d13f15de79b801ea5a5da
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-08-20 15:27:53 +0100

    fix about me content

--- src/components/Homepage/AboutMe/AboutMe.js
+++ src/components/Homepage/AboutMe/AboutMe.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./_aboutMe.scss";
 import aboutImg from "../../../assets/images/aboutMe.png";
 import contact from "../../../assets/images/contact.png";
+import { Link } from "react-router-dom";
+import * as routes from "../../../routePaths";
 
 const AboutMe = props => (
   <section id="aboutMe" className="aboutMe container-fluid section-spacing">
@@ -19,30 +21,28 @@ const AboutMe = props => (
           <div className="row ml-0 about-lists-row">
             <ul className="col-12 col-lg-6 about-list">
               <li className="description">Developer Relations</li>
-             <p>Lead teams in DevRel & Managed high scale programs in DevRel acorss Africa.</p>
+             <p>I have Lead DevRel teams & Managed high scale programs in DevRel acorss Africa, positioning a product at a point of a unique developer onboarding experience.</p>
             </ul>
             <ul className="col-12 col-lg-6 about-list">
               <li className="description">Engineering</li>
-              <p>Over 8 years experience building complex systems from the Web to mobile and Desktop side of engineering, and as a Developer Advocate, working acoross technologies. </p>
+              <p>Over 8 years experience building complex systems from the web to mobile and desktop side of engineering, and as a Developer Advocate, working acoross many technologies. </p>
             </ul>
             <ul className="col-12 col-lg-6 about-list">
-              <li className="description">Problem Solving</li>
-              <li className="description">Finding the Right Solution</li>
-              <li className="description">Finding the Right Solution</li>
-              <li className="description">Finding the Right Solution</li>
+            <li className="description">Program Manager</li>
+              <p>I have managed highscale programs for companies based on contract, my goal was to come up with a program that would help onboard more developers into a cool new pltaform.</p>
             </ul>
             <ul className="col-12 col-lg-6 about-list">
-              <li className="description">Problem Solving</li>
-              <li className="description">Finding the Right Solution</li>
-              <li className="description">Finding the Right Solution</li>
-              <li className="description">Finding the Right Solution</li>
+              <li className="description">Community Manager</li>
+              <p>Starting, building and scaling communities is exciting, organizing conferences or meetups to empower the people in the community with technology has always been exciting to me.</p>
             </ul>
           </div>
           <div className="contact-me-wrapper">
+          <Link to={`${routes.homepage}#contactUs`} className="nav-link">
             <button className="primary-btn">
               <img src={contact} alt="contact" />
               Contact Me
             </button>
+            </Link>
           </div>
         </div>
         <div className="col-12 col-lg-7 about-img-col">
